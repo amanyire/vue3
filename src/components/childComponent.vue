21a8b7e02229e4ba0dff75951628e61f80685769
patrick Maylean <pacmac2013@gmail.com>
2025-07-03 18:33:57 +0300

    Setting up the project

---
+++ src/components/childComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <button @click="handleClick">Click Me</button>
+</template>
+<script setup>
+const emit = defineEmits(['custom-click'])
+
+const handleClick = () => {
+  emit('custom-click') // Emits to parent
+}
+</script>
---
+++ src/components/parentComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="increment">Increment Value</button>
+    <h1>
+      HOME EDITS {{ counter }}
+      Array X {{ numbersArray }}
+    </h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const counter = ref(0)
+const numbersArray = ref([])
+function increment (){
+  counter.value++
+  numbersArray.value.push(counter.value)
+}
+
+</script>
