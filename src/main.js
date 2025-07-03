0d64805b84286bfc19d94b92870024170dbef82d
patrick Maylean <pacmac2013@gmail.com>
2025-07-03 20:59:59 +0300

    Added vue routing for testing

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import home from './components/home.vue'
+import about from './components/about.vue'
+import theWorld from './components/TheWelcome.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', component: home},
+        {path: '/about', component: about},
+        {path: '/theworld', component: theWorld}
+    ]
+});
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app')
